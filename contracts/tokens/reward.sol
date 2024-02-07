b231205769c8282fa7711542fac48195adcad802
Yuriy <berlandspw@gmail.com>
2024-02-07 08:25:43 +0500

    Reward contract

---
+++ contracts/tokens/reward.sol
@@ -0,0 +1,182 @@
+pragma solidity ^0.8.0;
+
+abstract contract Context {
+    function _msgSender() internal view virtual returns (address) {
+        return msg.sender;
+    }
+
+    function _msgData() internal view virtual returns (bytes calldata) {
+        return msg.data;
+    }
+}
+
+abstract contract Ownable is Context {
+    address private _owner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    /**
+     * @dev Initializes the contract setting the deployer as the initial owner.
+     */
+    constructor() {
+        _setOwner(_msgSender());
+    }
+
+    /**
+     * @dev Returns the address of the current owner.
+     */
+    function owner() public view virtual returns (address) {
+        return _owner;
+    }
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        require(owner() == _msgSender(), "Ownable: caller is not the owner");
+        _;
+    }
+
+    /**
+     * @dev Leaves the contract without owner. It will not be possible to call
+     * `onlyOwner` functions anymore. Can only be called by the current owner.
+     *
+     * NOTE: Renouncing ownership will leave the contract without an owner,
+     * thereby removing any functionality that is only available to the owner.
+     */
+    function renounceOwnership() public virtual onlyOwner {
+        _setOwner(address(0));
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Can only be called by the current owner.
+     */
+    function transferOwnership(address newOwner) public virtual onlyOwner {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        _setOwner(newOwner);
+    }
+
+    function _setOwner(address newOwner) private {
+        address oldOwner = _owner;
+        _owner = newOwner;
+        emit OwnershipTransferred(oldOwner, newOwner);
+    }
+}
+
+abstract contract ERC20Interface {
+    function totalSupply() public virtual view returns (uint);
+    function balanceOf(address tokenOwner) public virtual  view returns (uint balance);
+    function allowance(address tokenOwner, address spender) public virtual  view returns (uint remaining);
+    function transfer(address to, uint tokens) public virtual  returns (bool success);
+    function approve(address spender, uint tokens) public virtual  returns (bool success);
+    function transferFrom(address from, address to, uint tokens) public virtual  returns (bool success);
+
+    event Transfer(address indexed from, address indexed to, uint tokens);
+    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+}
+
+// ----------------------------------------------------------------------------
+// Safe Math Library
+// ----------------------------------------------------------------------------
+contract SafeMath {
+    function safeAdd(uint a, uint b) public pure returns (uint c) {
+        c = a + b;
+        require(c >= a);
+    }
+    function safeSub(uint a, uint b) public pure returns (uint c) {
+        require(b <= a); c = a - b; } function safeMul(uint a, uint b) public pure returns (uint c) { c = a * b; require(a == 0 || c / a == b); } function safeDiv(uint a, uint b) public pure returns (uint c) { require(b > 0);
+        c = a / b;
+    }
+}
+
+library TransferHelper {
+    function safeApprove(
+        address token,
+        address to,
+        uint256 value
+    ) internal {
+        // bytes4(keccak256(bytes('approve(address,uint256)')));
+        (bool success, bytes memory data) = token.call(abi.encodeWithSelector(0x095ea7b3, to, value));
+        require(
+            success && (data.length == 0 || abi.decode(data, (bool))),
+            'TransferHelper::safeApprove: approve failed'
+        );
+    }
+
+    function safeTransfer(
+        address token,
+        address to,
+        uint256 value
+    ) internal {
+        // bytes4(keccak256(bytes('transfer(address,uint256)')));
+        (bool success, bytes memory data) = token.call(abi.encodeWithSelector(0xa9059cbb, to, value));
+        require(
+            success && (data.length == 0 || abi.decode(data, (bool))),
+            'TransferHelper::safeTransfer: transfer failed'
+        );
+    }
+
+    function safeTransferFrom(
+        address token,
+        address from,
+        address to,
+        uint256 value
+    ) internal {
+        // bytes4(keccak256(bytes('transferFrom(address,address,uint256)')));
+        (bool success, bytes memory data) = token.call(abi.encodeWithSelector(0x23b872dd, from, to, value));
+        require(
+            success && (data.length == 0 || abi.decode(data, (bool))),
+            'TransferHelper::transferFrom: transferFrom failed'
+        );
+    }
+
+    function safeTransferETH(address to, uint256 value) internal {
+        (bool success, ) = to.call{value: value}(new bytes(0));
+        require(success, 'TransferHelper::safeTransferETH: ETH transfer failed');
+    }
+}
+
+contract RewardSender is SafeMath, Ownable {
+   
+   address public rewardToken;
+   address public gameAdmin;
+   uint256 public rewardSize;
+
+   mapping(address => uint) balances;
+   mapping(address => uint) rewardCount;
+
+   constructor(
+      address _admin,
+      address _token,
+      uint256 _rewardSize
+   ) {
+      rewardToken = _token;
+      gameAdmin = _admin;
+      rewardSize = _rewardSize;
+   }
+
+   modifier onlyAdmin() {
+        require(gameAdmin == _msgSender(), "Caller is not have enough rights");
+        _;
+    }
+   
+   function setAdmin (address _newAdmin) external onlyOwner {
+       gameAdmin = _newAdmin;
+   }
+
+   function updateRewardSize (uint256 _newRewardSize) external onlyOwner {
+       rewardSize = _newRewardSize;
+   }
+
+   function noteWinner (address _winner) external onlyAdmin {
+      balances[_winner] += rewardSize;
+      rewardCount[_winner] += 1;
+   }
+
+   function withdrawRewards () external {
+      require(balances[msg.sender] > 0, 'Caller has a zero balance');
+      TransferHelper.safeTransfer(rewardToken, msg.sender, balances[msg.sender]);
+      balances[msg.sender] = 0;
+   }
+}
