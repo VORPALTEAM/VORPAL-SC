ce5947e147c8cedf3091aef568805ffaf2f412ea
Yuriy <berlandspw@gmail.com>
2024-02-24 21:50:00 +0500

    Additiona random provider

--- contracts/ecosystem/RandomProvider.sol
+++ contracts/ecosystem/RandomProvider.sol
@@ -14,3 +14,18 @@ abstract contract RandomProvider {
     }
 
 }
+
+abstract contract RandomProviderPublic {
+
+    uint256 private hash = 0;
+
+   function UpdateRandom() public {
+         hash = uint256(keccak256(abi.encodePacked(block.difficulty, block.timestamp)));
+    }
+
+    function GetRandomValue (uint32 _depth) external view returns (uint) {
+        uint _randomPercent = hash % (10 ** _depth);
+        return _randomPercent;
+    }
+
+}
